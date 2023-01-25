c502596b133d91f9eaecb98d87db54b8e5464d24
José Juan Sánchez <josejuan.sanchez@gmail.com>
2023-01-25 14:32:24 +0100

    Se añade la clave ssh a las instancias

--- ejemplos/ejemplo-01/main.tf
+++ ejemplos/ejemplo-01/main.tf
@@ -7,6 +7,7 @@ provider "aws" {
 resource "aws_instance" "instancia_ejemplo_01" {
   ami           = "ami-00874d747dde814fa"
   instance_type = "t2.small"
+  key_name      = "vockey"
   tags = {
     Name = "instancia_ejemplo_01"
   }
